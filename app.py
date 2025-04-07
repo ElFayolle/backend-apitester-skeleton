0e13a6962a6b6e7dd000d4114447bb029be14b9d
Eliott Fayolle <eliott.fayolle@gmail.com>
2025-04-07 21:12:22 +0200

    associations et noms

--- app.py
+++ app.py
@@ -12,11 +12,28 @@ CORS(app)
 
 data = pl.Path(__file__).parent.absolute() / 'data'
 
+
 # Charger les données CSV
 associations_df = pd.read_csv(data / 'associations_etudiantes.csv')
 evenements_df = pd.read_csv(data / 'evenements_associations.csv')
 
-## Vous devez ajouter les routes ici : 
+@app.route('/api/alive', methods=['GET'])
+def alive():
+    return jsonify({"message": "Alive"}), 200
+
+@app.route('/api/associations', methods=['GET'])
+def get_associations():
+    return jsonify(associations_df['id'].tolist()), 200
+
+@app.route('/api/association/<int:id>', methods=['GET'])
+def get_association(id):
+    assoc = associations_df[associations_df['id'] == id]
+    if assoc.empty:
+        return jsonify({"error": "Association not found"}), 404
+    return jsonify(assoc.iloc[0].to_dict()), 200
+
+
+
 
 
 
